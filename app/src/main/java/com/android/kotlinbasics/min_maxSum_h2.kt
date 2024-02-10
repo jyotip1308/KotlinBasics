315a3b8b6b3b251abac293d84b048ed02ea4b77f
JYOTI PANDEY <jyotip9352@gmail.com>
2024-02-10 15:12:19 +0530

    min_maxSum-H2

--- app/src/main/java/com/android/kotlinbasics/min_maxSum_h2.kt
+++ app/src/main/java/com/android/kotlinbasics/min_maxSum_h2.kt
@@ -4,20 +4,54 @@ fun main(){
     val a = mData.a
     var max = a[0]
     var min = a[0]
-    var sum = 0
+    var maxsum = 0
+    var minsum = 0
+    var counter = 0
 
-    for (i in a){
+
+    a.sort()
+//    println(a)
+    a.forEach {
+        println(it)
+
+        if(counter<4){
+            minsum += it
+        }
+        if(counter>0){
+            maxsum += it
+        }
+        counter++
+
+    }
+
+
+
+/*    for (i in a){
         sum += i
+
         if(i > max){
             max = i
         }
         else if (i < min){
             min = i
         }
-    }
+    }*/
 
-    println(sum)
+
+
+   /*val m =  (sum-max)
+   val n =  (sum-min)*/
 
     println("Maximum value is: $max")
     println("Minimum Value is: $min")
+
+//    println("$m $n")
+    println("$minsum $maxsum")
+
+
+
+
+
 }
+
+
