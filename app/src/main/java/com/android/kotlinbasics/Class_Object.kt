c508e9419fd0fb74d443c80a3c58692a34ca73f9
JYOTI PANDEY <jyotip9352@gmail.com>
2024-04-02 16:56:31 +0530

    Class_Object

--- app/src/main/java/com/android/kotlinbasics/Class_Object.kt
+++ app/src/main/java/com/android/kotlinbasics/Class_Object.kt
@@ -1,18 +1,68 @@
 package com.android.kotlinbasics
 
 fun main(){
-    val car = Car()
-    println(car.color)
-    println(car.model)
+    val car = Car(color = "Green", model = "NJKD")
+    println("Color : ${car.color} Model : ${car.model}")
+
+
+    val secondCar = Car(color = "Purple", model = "dekep")
+    println("Color : ${secondCar.color} Model : ${secondCar.model}")
+
+    car.color = "Black"
+    car.model = "IUUE"
+    println("Color : ${car.color} Model : ${car.model}")
+
+    car.speed(30,40)
+    car.speed(maxSpeed = 89, minSpeed = 34)
+
     println(car.drive())
+
+
 }
 
-class Car{
-    val color: String = "Black"
-    val model: String = "XMD"
+//ONE WAY
+
+/*class Car(color: String = "Red", model: String = "JKHK"){
+ *//*   val color: String = "Black"
+    val model: String = "XMD"*//*
+
+    var color: String = color
+    var model: String = model
+
+    fun drive(){
+        println("Let's Drive.......")
+    }
+}*/
+
+
+//BETTER WAY
+
+class Car(var color: String, var model: String){
+    /*   val color: String = "Black"
+       val model: String = "XMD"*/
+
+    // This will change color, model of car and secondCar to orange and ij0ij
+ /*   init {
+        color = "Orange"
+        model = "ijoij"
+    }*/
+
+   init {
+       if (color =="Green")
+           println("This is Green")
+
+       else{
+           println("This $color is not green")
+       }
+   }
+
+    fun speed(minSpeed:Int, maxSpeed:Int){
+        println("MinSpeed is $minSpeed and maxSpeed is $maxSpeed")
+    }
 
     fun drive(){
         println("Let's Drive.......")
     }
 }
+
 
